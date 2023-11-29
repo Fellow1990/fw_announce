07d3870107d50a1acf68c562d26ae7d0528e15fb
Fellow25 <92899080+Fellow25@users.noreply.github.com>
2023-11-29 23:04:23 +0000

    First release

---
+++ shared/config.lua
@@ -0,0 +1,63 @@
+Config                            = {}
+
+Config.Context = false
+Config.Position = 'top-right' -- 'top-left' or 'top-right' or 'bottom-left' or 'bottom-right'
+
+Config.Announce = {
+    ['mechanic'] = {
+        ['Ouvert'] = {
+            id                              = 'Ouvert',
+            title                           = "Informations",
+            description                     = "Ouvert",
+            position                        = 'top',
+            style = {
+                backgroundColor             = '#000000',
+                color                       = '#ffffff'
+            },
+            icon                            = 'box',
+            type                            = 'error',
+            iconColor                       = '#FFFFFF'
+        },
+        ['Fermer'] = {
+            id                              = 'Fermer',
+            title                           = "Informations",
+            description                     = "Fermer",
+            position                        = 'top',
+            style = {
+                backgroundColor             = '#000000',
+                color                       = '#ffffff'
+            },
+            icon                            = 'car',
+            type                            = 'error',
+            iconColor                       = '#FFFFFF'
+        },
+    },
+    ['cardealer'] = {
+        ['Ouvert 1'] = {
+            id                              = 'Ouvert',
+            title                           = "Informations",
+            description                     = "Ouvert",
+            position                        = 'top',
+            style = {
+                backgroundColor             = '#000000',
+                color                       = '#ffffff'
+            },
+            icon                            = 'box',
+            type                            = 'error',
+            iconColor                       = '#FFFFFF'
+        },
+        ['Fermer 1'] = {
+            id                              = 'Fermer',
+            title                           = "Informations",
+            description                     = "Fermer",
+            position                        = 'top',
+            style = {
+                backgroundColor             = '#000000',
+                color                       = '#ffffff'
+            },
+            icon                            = 'car',
+            type                            = 'error',
+            iconColor                       = '#FFFFFF'
+        },
+    }
+}
